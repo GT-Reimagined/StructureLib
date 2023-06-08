fcb5edf025e8d52faf6b784bd6814d2efdc16bc2
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-06-08 18:35:21 -0400

    fixed command registration

--- src/main/java/com/gtnewhorizon/structurelib/command/CommandConfigureChannels.java
+++ src/main/java/com/gtnewhorizon/structurelib/command/CommandConfigureChannels.java
@@ -1,121 +1,104 @@
 package com.gtnewhorizon.structurelib.command;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import net.minecraft.command.CommandBase;
-import net.minecraft.command.ICommandSender;
-import net.minecraft.command.WrongUsageException;
-import net.minecraft.entity.player.ServerPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.TranslatableComponent;
-
+import com.gtnewhorizon.structurelib.StructureLibAPI;
 import com.gtnewhorizon.structurelib.alignment.constructable.ChannelDataAccessor;
 import com.gtnewhorizon.structurelib.item.ItemConstructableTrigger;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
-public class CommandConfigureChannels extends CommandBase {
+@Mod.EventBusSubscriber(modid = StructureLibAPI.MOD_ID)
+public class CommandConfigureChannels {
 
-    @Override
-    public String getCommandName() {
-        return "sl_channel";
+    private static DynamicCommandExceptionType INVALID_ITEM = new DynamicCommandExceptionType(s -> new TextComponent("must hold a hologram"));
+
+    public static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher, Commands.CommandSelection selection) {
+        dispatcher.register(Commands.literal("sl_channel")
+            .then(Commands.literal("get")
+                .then(Commands.argument("channel_name", StringArgumentType.greedyString()))
+                .executes(context -> processCommand(context, SubCommand.GET)))
+            .then(Commands.literal("set")
+                .then(Commands.argument("channel_name", StringArgumentType.greedyString())
+                    .then(Commands.argument("value", IntegerArgumentType.integer(1))
+                        .executes(context -> processCommand(context, SubCommand.SET)))))
+            .then(Commands.literal("unset")
+                .then(Commands.argument("channel_name", StringArgumentType.greedyString())
+                    .executes(context -> processCommand(context, SubCommand.UNSET))))
+            .then(Commands.literal("wipe").executes(context -> processCommand(context, SubCommand.WIPE)))
+            .then(Commands.literal("getall").executes(context -> processCommand(context, SubCommand.GETALL))));
     }
 
-    @Override
-    public String getCommandUsage(ICommandSender p_71518_1_) {
-        return "/sl_channel <wipe/getall>\n" + "/sl_channel <get/unset> <channel name>\n"
-                + "/sl_channel <set> <channel name> <value>\n"
-                + "<channel name> can have space in it. <value> must be positive integer";
-    }
-
-    @Override
-    public int getRequiredPermissionLevel() {
-        return 0;
-    }
-
-    @Override
-    public List<?> addTabCompletionOptions(ICommandSender p_71516_1_, String[] p_71516_2_) {
-        if (p_71516_2_.length == 1)
-            return getListOfStringsMatchingLastWord(p_71516_2_, "get", "set", "unset", "wipe", "getall");
-        return null;
-    }
-
-    @Override
-    public void processCommand(ICommandSender sender, String[] args) {
-        if (args.length == 0) throw new WrongUsageException(getCommandUsage(sender));
-
-        if (!(sender instanceof ServerPlayer)) throw new WrongUsageException("must be a player");
-
-        ServerPlayer player = (ServerPlayer) sender;
-
-        ItemStack heldItem = player.getHeldItem();
-        if (heldItem.isEmpty() || !(heldItem.getItem() instanceof ItemConstructableTrigger)) {
-            throw new WrongUsageException("must hold a hologram");
+    private static int processCommand(CommandContext<CommandSourceStack> context, SubCommand command) throws CommandSyntaxException {
+        ServerPlayer player = context.getSource().getPlayerOrException();
+        ItemStack heldStack = player.getMainHandItem();
+        if (heldStack.isEmpty() || !(heldStack.getItem() instanceof ItemConstructableTrigger)){
+            throw INVALID_ITEM.create(null);
         }
-
-        String channel;
-
-        switch (args[0]) {
-            case "getall":
-                if (!ChannelDataAccessor.hasSubChannel(heldItem)) {
-                    sender.addChatMessage(new ChatComponentText("No subchannel"));
+        return switch (command){
+            case GET -> {
+                String channel = context.getArgument("channel_name", String.class);
+                if (ChannelDataAccessor.hasSubChannel(heldStack, channel)){
+                    player.sendMessage(new TextComponent(channel + " value: " + ChannelDataAccessor.getChannelData(heldStack, channel)), player.getUUID());
+                    yield 1;
+                }
+                player.sendMessage(new TextComponent(channel + " value: N/A"), player.getUUID());
+                yield 0;
+            }
+            case SET -> {
+                String channel = context.getArgument("channel_name", String.class);
+                int value = context.getArgument("value", Integer.class);
+                ChannelDataAccessor.setChannelData(heldStack, channel, value);
+                player.containerMenu.sendAllDataToRemote();
+                player.sendMessage(new TextComponent(channel + " value: " + value), player.getUUID());
+                yield 1;
+            }
+            case UNSET -> {
+                String channel = context.getArgument("channel_name", String.class);
+                if (ChannelDataAccessor.hasSubChannel(heldStack, channel)){
+                    ChannelDataAccessor.unsetChannelData(heldStack, channel);
+                    player.sendMessage(new TextComponent(channel + " cleared"), player.getUUID());
+                    yield 1;
+                }
+                player.sendMessage(new TextComponent(channel + " no value"), player.getUUID());
+                yield 0;
+            }
+            case WIPE -> {
+                ChannelDataAccessor.wipeChannelData(heldStack);
+                yield 1;
+            }
+            case GETALL -> {
+                if (!ChannelDataAccessor.hasSubChannel(heldStack)) {
+                    player.sendMessage(new TextComponent("No subchannel"), player.getUUID());
                 } else {
-                    sender.addChatMessage(
-                            new TranslatableComponent(
-                                    "item.structurelib.constructableTrigger.desc.lshift.0",
-                                    ChannelDataAccessor.countChannelData(heldItem)));
-                    ChannelDataAccessor.iterateChannelData(heldItem).map(e -> e.getKey() + ": " + e.getValue())
-                            .map(ChatComponentText::new).forEach(player::addChatMessage);
+                    player.sendMessage(new TranslatableComponent(
+                        "item.structurelib.constructableTrigger.desc.lshift.0",
+                        ChannelDataAccessor.countChannelData(heldStack)), player.getUUID());
+                    ChannelDataAccessor.iterateChannelData(heldStack).map(e -> e.getKey() + ": " + e.getValue())
+                        .map(TextComponent::new).forEach(c -> player.sendMessage(c, player.getUUID()));
                 }
-                return;
-            case "wipe":
-                ChannelDataAccessor.wipeChannelData(heldItem);
-                return;
-            case "get":
-            case "unset":
-                if (args.length < 2) throw new WrongUsageException(getCommandUsage(sender));
-                channel = args.length == 2 ? args[1] : Arrays.stream(args).skip(1).collect(Collectors.joining());
-                break;
-            case "set":
-                if (args.length < 3) throw new WrongUsageException(getCommandUsage(sender));
-                channel = args.length == 3 ? args[1]
-                        : Arrays.stream(args).skip(1).limit(args.length - 2).collect(Collectors.joining());
-                break;
-            default:
-                throw new WrongUsageException(getCommandUsage(sender));
-        }
+                yield 1;
+            }
+        };
+    }
 
-        switch (args[0]) {
-            case "get":
-                if (ChannelDataAccessor.hasSubChannel(heldItem, channel)) sender.addChatMessage(
-                        new ChatComponentText(
-                                channel + " value: " + ChannelDataAccessor.getChannelData(heldItem, channel)));
-                else sender.addChatMessage(new ChatComponentText(channel + " value: N/A"));
-                break;
-            case "clear":
-                if (ChannelDataAccessor.hasSubChannel(heldItem, channel))
-                    sender.addChatMessage(new ChatComponentText(channel + " no value"));
-                else {
-                    ChannelDataAccessor.unsetChannelData(heldItem, channel);
-                    sender.addChatMessage(new ChatComponentText(channel + " cleared"));
-                    player.inventoryContainer.detectAndSendChanges();
-                }
-                break;
-            case "set":
-                int value;
-                try {
-                    value = Integer.parseInt(args[args.length - 1]);
-                } catch (NumberFormatException e) {
-                    throw new WrongUsageException("%s not a valid value", args[args.length - 1]);
-                }
+    enum SubCommand{
+        GET, SET, UNSET, WIPE, GETALL;
+    }
 
-                if (value <= 0) throw new WrongUsageException("%s not a valid value", args[args.length - 1]);
-
-                ChannelDataAccessor.setChannelData(heldItem, channel, value);
-                player.inventoryContainer.detectAndSendChanges();
-                sender.addChatMessage(new ChatComponentText(channel + " value: " + value));
-                break;
-        }
+    @SubscribeEvent
+    public static void onCommandRegistration(RegisterCommandsEvent event){
+        registerCommands(event.getDispatcher(), event.getEnvironment());
     }
 }
