8cfeef12e8eaf3f4f3274393495d1a5259f66d0b
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-06-14 12:08:41 -0400

    forgot to register world load event on forge side

--- forge/src/main/java/com/gtnewhorizon/structurelib/forge/StructureLibForge.java
+++ forge/src/main/java/com/gtnewhorizon/structurelib/forge/StructureLibForge.java
@@ -8,6 +8,8 @@ import com.gtnewhorizon.structurelib.command.CommandConfigureChannels;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -15,6 +17,7 @@ import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(StructureLibAPI.MOD_ID)
 public class StructureLibForge extends StructureLib {
@@ -28,6 +31,9 @@ public class StructureLibForge extends StructureLib {
         FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Block.class, this::onRegisterBlock);
         MinecraftForge.EVENT_BUS.addListener(this::onCommandRegistration);
         preInit();
+        if (FMLEnvironment.dist.isClient()){
+            MinecraftForge.EVENT_BUS.addListener(this::onWorldLoad);
+        }
     }
 
     private void onRegisterBlock(RegistryEvent.Register<Block> event){
@@ -38,6 +44,7 @@ public class StructureLibForge extends StructureLib {
         CommandConfigureChannels.registerCommands(event.getDispatcher(), event.getEnvironment());
     }
 
+    @OnlyIn(Dist.CLIENT)
     private void onWorldLoad(WorldEvent.Load event){
         ClientProxy.onLevelLoad(event.getWorld());
     }
