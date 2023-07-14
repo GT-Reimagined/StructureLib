5b5b19c612b36b8f4e9e7602d224706a3085b1ed
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-07-14 00:56:35 -0400

    made constructable trigger handle multiblocks

--- common/src/main/java/com/gtnewhorizon/structurelib/item/ItemConstructableTrigger.java
+++ common/src/main/java/com/gtnewhorizon/structurelib/item/ItemConstructableTrigger.java
@@ -13,6 +13,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
@@ -20,6 +21,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
@@ -36,6 +38,11 @@ public class ItemConstructableTrigger extends Item implements MenuProvider {
             return InteractionResultHolder.success(player.getItemInHand(usedHand));
         }
         return super.use(level, player, usedHand);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        return ConstructableUtility.handle(context.getItemInHand(), context.getPlayer(), context.getLevel(), context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ(), context.getClickedFace().get3DDataValue()) ? InteractionResult.SUCCESS : InteractionResult.PASS;
     }
 
     @Override
